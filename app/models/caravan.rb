db37f3ea6c3d4856cecc3c6b9487aca257a8908d
Thibault Paulmann <thibault.paulmann@gmail.com>
2022-08-22 15:25:15 +0200

    completed validations for caravan and booking

--- app/models/caravan.rb
+++ app/models/caravan.rb
@@ -3,7 +3,7 @@ class Caravan < ApplicationRecord
 
   belongs_to :user, dependent: :destroy
 
-  validates :model, presence: true, length { minimum: 3 }
+  validates :model, presence: true, length: { minimum: 3 }
   validates :capacity, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :location, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
