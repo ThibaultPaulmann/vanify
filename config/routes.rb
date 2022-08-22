b59d8b5f50fa886acc8ef67d2502ed4d7c74eab8
satoshimikoto <njrmikeli@gmail.com>
2022-08-22 15:30:18 +0200

    routes  addded

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :caravans, only: [:index, :show, :new, :create, :update, :destroy]
 end
