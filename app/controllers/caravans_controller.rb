56b0ec1ad35d01b1c3aa7cd2afbb5a7a2d3ba4e3
satoshimikoto <njrmikeli@gmail.com>
2022-08-23 16:36:44 +0200

    beta version od adding photo function emplemented

--- app/controllers/caravans_controller.rb
+++ app/controllers/caravans_controller.rb
@@ -4,7 +4,7 @@ class CaravansController < ApplicationController
   def index
     @caravans = Caravan.all
   end
-  
+
   def show
     @caravan = Caravan.find(params[:id])
   end
@@ -18,11 +18,11 @@ class CaravansController < ApplicationController
     @caravan.save
     redirect_to caravans_path
   end
-  
+
   def edit
     @caravan =  Caravan.find(params[:id])
   end
-  
+
   def update
     @caravan =  Caravan.find(params[:id])
     @caravan.update(caravan_params)
@@ -38,7 +38,7 @@ class CaravansController < ApplicationController
   private
 
   def caravan_params
-    params.require(:caravan).permit(:model, :photo)
+    params.require(:caravan).permit(:model, photos: [])
   end
 
 end
