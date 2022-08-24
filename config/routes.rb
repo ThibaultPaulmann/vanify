c7fcb633493879a7a1cf84d10f3b1736a2811cfb
Philipp Glasmacher <philipp@glasmacher.com>
2022-08-24 18:10:34 +0200

    started working on the showpage

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,5 @@ Rails.application.routes.draw do
     resources :bookings, only: [:create]
   end
 
-  resources :bookings, only: [:index, :destroy]
-
+  resources :bookings, only: [:index, :show, :destroy]
 end
