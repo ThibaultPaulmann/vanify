172179084ba807ad6c7aa3c07a4b7f873bea28a4
Thibault Paulmann <thibault.paulmann@gmail.com>
2022-08-25 16:28:04 +0200

    fixed bug

--- app/javascript/controllers/booking_card_controller.js
+++ app/javascript/controllers/booking_card_controller.js
@@ -1,14 +1,10 @@
 import { Controller } from "@hotwired/stimulus";
-import { initFlatPickr } from "app/javascript/plugins/init_flatpickr.js";
+import { initFlatPickr } from "../plugins/init_flatpickr";
 
 // Connects to data-controller="booking-card"
 export default class extends Controller {
   connect() {
     console.log("connected to booking card");
     initFlatPickr();
-  }
-
-  click() {
-    console.log("clicked");
   }
 }
